6b23a021c24be237f26268225e23d8fac4fb39a5
Harley Laue <losinggeneration@gmail.com>
2012-05-17 10:46:11 -0500

    Added implementation for Explosion spatial

--- include/spatials/Explosion.h
+++ include/spatials/Explosion.h
@@ -1,12 +1,27 @@
 #ifndef SPACEWARRIOR_EXPLOSION_H
 #define SPACEWARRIOR_EXPLOSION_H
 
+#include "SDL.h"
 #include "spatials/Spatial.h"
 
 namespace StarWarrior {
 
-class Explosion : public Spatial {
+class Expires;
+class Transform;
 
+class Explosion : public Spatial {
+public:
+	Explosion(hecate::World *world, hecate::Entity *owner, int radius);
+	void initalize();
+	void render(SDL_Surface *s);
+
+private:
+	Transform *transform;
+	Expires *expires;
+	SDL_Color color;
+	Uint8 alpha;
+	int initialLifeTime;
+	int radius;
 };
 
 }
--- src/spatials/Explosion.cpp
+++ src/spatials/Explosion.cpp
@@ -1,1 +1,36 @@
 #include "spatials/Explosion.h"
+
+#include "components/Expires.h"
+#include "components/Transform.h"
+
+#include "hecate/ComponentMapper.h"
+
+#include "SDL_gfxPrimitives.h"
+
+namespace StarWarrior {
+
+using namespace hecate;
+
+Explosion::Explosion(World *w, Entity *e, int r) : Spatial(w, e), radius(r) {
+}
+
+void Explosion::initalize() {
+	ComponentMapper<Transform> transformMapper(transform, world);
+	transform = transformMapper.get(*owner);
+
+	ComponentMapper<Expires> expiresMapper(expires, world);
+	expires = expiresMapper.get(*owner);
+	initialLifeTime = expires->getLifeTime();
+
+	color.r = 255;
+	color.g = 255;
+	color.b = 0;
+}
+
+void Explosion::render(SDL_Surface *s) {
+	alpha = (float)expires->getLifeTime()/(float)initialLifeTime;
+
+	filledEllipseRGBA(s, transform->getX() - radius, transform->getY() - radius, radius*2, radius*2, color.r, color.g, color.b, alpha);
+}
+
+}
