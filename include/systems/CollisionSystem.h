05bacb6833d3cc269c9b541662d3db71f29df41d
Harley Laue <losinggeneration@gmail.com>
2012-05-17 16:24:56 -0500

    Partial implementation for CollisionSystem
    * It looks like Component, ComponentMapper, and ComponentTypeManager may
      need to be reworked in order to work as they do in Artemis.

--- include/systems/CollisionSystem.h
+++ include/systems/CollisionSystem.h
@@ -1,10 +1,30 @@
 #ifndef STARWARRIOR_COLLISIONSYSTEM_H
 #define STARWARRIOR_COLLISIONSYSTEM_H
 
+#include "hecate/ComponentMapper.h"
+#include "hecate/EntitySystem.h"
+
 namespace StarWarrior {
 
-class CollisionSystem {
+class Health;
+class Transform;
+class Velocity;
 
+class CollisionSystem : public hecate::EntitySystem {
+public:
+	CollisionSystem();
+	void initialize();
+
+protected:
+	void processEntities(std::set<hecate::Entity *> entities);
+	bool checkProcessing();
+
+private:
+	bool collisionExists(hecate::Entity *e1, hecate::Entity *e2);
+
+	hecate::ComponentMapper<Transform> *transformMapper;
+	hecate::ComponentMapper<Velocity> *velocityMapper;
+	hecate::ComponentMapper<Health> *healthMapper;
 };
 
 }
--- src/systems/CollisionSystem.cpp
+++ src/systems/CollisionSystem.cpp
@@ -1,1 +1,38 @@
 #include "systems/CollisionSystem.h"
+
+#include "components/Health.h"
+#include "components/Transform.h"
+#include "components/Velocity.h"
+
+namespace StarWarrior {
+
+using namespace hecate;
+
+CollisionSystem::CollisionSystem() {
+}
+
+void CollisionSystem::initialize() {
+	Transform t;
+	Velocity v;
+	Health h(100);
+
+	transformMapper = new ComponentMapper<Transform>(&t, world);
+	velocityMapper = new ComponentMapper<Velocity>(&v, world);
+	healthMapper = new ComponentMapper<Health>(&h, world);
+}
+
+void CollisionSystem::processEntities(std::set<hecate::Entity *> entities) {
+}
+
+bool CollisionSystem::checkProcessing() {
+	return true;
+}
+
+bool CollisionSystem::collisionExists(hecate::Entity *e1, hecate::Entity *e2) {
+	Transform *t1 = transformMapper->get(*e1);
+	Transform *t2 = transformMapper->get(*e2);
+
+	return t1->getDistanceTo(*t2) < 15;
+}
+
+}
