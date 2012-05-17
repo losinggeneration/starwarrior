0e0e86eb63e17945f0f9c6055a4771f6eb391f90
Harley Laue <losinggeneration@gmail.com>
2012-05-17 11:00:36 -0500

    Added implementation for Missile spatial

--- include/spatials/Missile.h
+++ include/spatials/Missile.h
@@ -1,12 +1,21 @@
 #ifndef SPACEWARRIOR_MISSILE_H
 #define SPACEWARRIOR_MISSILE_H
 
+#include "SDL.h"
 #include "spatials/Spatial.h"
 
 namespace StarWarrior {
 
-class Missile : public Spatial {
+class Transform;
 
+class Missile : public Spatial {
+public:
+	Missile(hecate::World *world, hecate::Entity *owner);
+	void initialize();
+	void render(SDL_Surface *s);
+
+private:
+	Transform *transform;
 };
 
 }
--- src/spatials/Missile.cpp
+++ src/spatials/Missile.cpp
@@ -1,1 +1,25 @@
 #include "spatials/Missile.h"
+
+#include "components/Transform.h"
+
+#include "hecate/ComponentMapper.h"
+
+#include "SDL_gfxPrimitives.h"
+
+namespace StarWarrior {
+
+using namespace hecate;
+
+Missile::Missile(World *w, Entity *e) : Spatial(w, e) {
+}
+
+void Missile::initialize() {
+	ComponentMapper<Transform> transformMapper(transform, world);
+	transform = transformMapper.get(*owner);
+}
+
+void Missile::render(SDL_Surface *s) {
+	rectangleRGBA(s, transform->getX(), transform->getY(), transform->getX() + 2, transform->getY() + 6, 255, 255, 255, 0);
+}
+
+}
