36b2bbe775d9e30a1a1eee1afac3ef779a6938f6
Harley Laue <losinggeneration@gmail.com>
2012-05-18 17:11:53 -0500

    Stub for EnemyShipMovementSystem

--- include/systems/EnemyShipMovementSystem.h
+++ include/systems/EnemyShipMovementSystem.h
@@ -1,10 +1,32 @@
 #ifndef STARWARRIOR_ENEMYSHIPMOVEMENTSYSTEM_H
 #define STARWARRIOR_ENEMYSHIPMOVEMENTSYSTEM_H
 
+#include "hecate/ComponentMapper.h"
+#include "hecate/Entity.h"
+#include "hecate/EntityProcessingSystem.h"
+
 namespace StarWarrior {
 
-class EnemyShipMovementSystem {
+class Enemy;
+class Transform;
+class Velocity;
 
+class EnemyShipMovementSystem : public hecate::EntityProcessingSystem<> {
+public:
+	EnemyShipMovementSystem(/*GameContainer container*/);
+	void initialize();
+
+protected:
+	void process(hecate::Entity *e);
+
+private:
+	// 	GameContainer container;
+	hecate::ComponentMapper<Transform> *transformMapper;
+	hecate::ComponentMapper<Velocity> *velocityMapper;
+
+	Enemy *e;
+	Transform *t;
+	Velocity *v;
 };
 
 }
--- src/systems/EnemyShipMovementSystem.cpp
+++ src/systems/EnemyShipMovementSystem.cpp
@@ -1,1 +1,19 @@
 #include "systems/EnemyShipMovementSystem.h"
+
+#include "components/Transform.h"
+
+using namespace hecate;
+
+namespace StarWarrior {
+
+EnemyShipMovementSystem::EnemyShipMovementSystem(/*GameContainer container*/) :
+EntityProcessingSystem(t, std::set<Component*>()){
+}
+
+void EnemyShipMovementSystem::initialize() {
+}
+
+void EnemyShipMovementSystem::process(Entity *e) {
+}
+
+}
