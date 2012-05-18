d95cfb1efe3a0065446b6b3462f796a81b8b9568
Harley Laue <losinggeneration@gmail.com>
2012-05-18 13:04:09 -0500

    Mostly finish CollisionSystem
    * Only thing missing is the EntityFactory parts which are commented out
      for now. So minus having an explosion animation happen, it should
      work...

--- src/systems/CollisionSystem.cpp
+++ src/systems/CollisionSystem.cpp
@@ -4,6 +4,9 @@
 #include "components/Transform.h"
 #include "components/Velocity.h"
 
+#include "hecate/Entity.h"
+#include "hecate/GroupManager.h"
+
 namespace StarWarrior {
 
 using namespace hecate;
@@ -22,6 +25,37 @@ void CollisionSystem::initialize() {
 }
 
 void CollisionSystem::processEntities(std::set<hecate::Entity *> entities) {
+	entitySet_t bullets = world->getGroupManager()->getEntities("BULLETS");
+	entitySet_t ships = world->getGroupManager()->getEntities("SHIPS");
+
+	if(!bullets.empty() && !ships.empty()) {
+		for(entitySet_t::iterator it = ships.begin(); it != ships.end(); it++) {
+			Entity *ship = *it;
+
+			for(entitySet_t::iterator bt = bullets.begin(); bt != bullets.end(); bt++) {
+				Entity *bullet = *bt;
+
+				if(collisionExists(bullet, ship)) {
+					Transform *tb = transformMapper->get(*bullet);
+// 					EntityFactory.createBulletExplosion(world, tb->getX(), tb->getY())->refresh();
+					world->deleteEntity(bullet);
+
+					Health *health = healthMapper->get(*ship);
+					health->addDamage(4);
+
+					if(!health->isAlive()) {
+						Transform *ts = transformMapper->get(*ship);
+
+// 						EntityFactory.createShipExplosion(world, ts->getX(), ts->getY())->refresh();
+
+						world->deleteEntity(ship);
+						//  break from the inner loop
+						break;
+					}
+				}
+			}
+		}
+	}
 }
 
 bool CollisionSystem::checkProcessing() {
