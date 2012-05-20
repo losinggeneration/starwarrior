0c2f956cd3142b9c8fb9323d63d2024d33a4bb04
Harley Laue <losinggeneration@gmail.com>
2012-05-20 06:48:08 -0500

    Implemented HudRenderSystem

--- include/systems/HudRenderSystem.h
+++ include/systems/HudRenderSystem.h
@@ -1,10 +1,28 @@
 #ifndef STARWARRIOR_HUDRENDERSYSTEM_H
-#define STARWARRIOR_
+#define STARWARRIOR_HUDRENDERSYSTEM_H
+
+#include "hecate/ComponentMapper.h"
+#include "hecate/EntityProcessingSystem.h"
+#include "SDL_ttf.h"
 
 namespace StarWarrior {
 
-class HudRenderSystem {
+class Health;
+class Player;
 
+class HudRenderSystem : public hecate::EntityProcessingSystem {
+private:
+	SDL_Surface *screen;
+	TTF_Font *font;
+	int height;
+	hecate::ComponentMapper<Health> healthMapper;
+
+public:
+	HudRenderSystem(SDL_Surface *screen, int height, TTF_Font *font);
+	void initialize();
+
+protected:
+	void process(hecate::Entity *e);
 };
 
 }
--- src/systems/HudRenderSystem.cpp
+++ src/systems/HudRenderSystem.cpp
@@ -1,1 +1,49 @@
 #include "systems/HudRenderSystem.h"
+
+#include "components/Health.h"
+#include "components/Player.h"
+
+#include <sstream>
+
+using namespace hecate;
+
+namespace StarWarrior {
+
+HudRenderSystem::HudRenderSystem(SDL_Surface *screen, int height, TTF_Font *font) :
+healthMapper(Health(), world) {
+	Health h;
+	Player p;
+	componentList_t l;
+
+	l.push_back(&h);
+	l.push_back(&p);
+	setupTypes(l);
+
+	this->screen = screen;
+	this->font = font;
+	this->height = height;
+}
+
+void HudRenderSystem::initialize() {
+}
+
+void HudRenderSystem::process(Entity *e) {
+	Health *health = healthMapper.get(*e);
+	std::stringstream ss;
+	std::string text;
+	SDL_Color white = {255,255,255};
+	SDL_Rect r;
+
+	ss << "Health: " << health->getHealthPercentage() << "%";
+	ss >> text;
+
+	SDL_Surface *render = TTF_RenderText_Solid(font, text.c_str(), white);
+	r.w = render->w;
+	r.h = render->h;
+	r.x = 20;
+	r.y = height - 40;
+
+	SDL_BlitSurface(screen, &r, render, NULL);
+}
+
+}
