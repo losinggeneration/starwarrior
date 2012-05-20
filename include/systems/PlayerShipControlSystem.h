10c4f40039077eba760c453ae288707fc6f50842
Harley Laue <losinggeneration@gmail.com>
2012-05-20 15:02:06 -0500

    Implement PlayerShipControlSystem
    * Input is dequeued from outside this class.

--- include/systems/PlayerShipControlSystem.h
+++ include/systems/PlayerShipControlSystem.h
@@ -1,10 +1,30 @@
 #ifndef STARWARRIOR_PLAYERSHIPCONTROLSYSTEM_H
 #define STARWARRIOR_PLAYERSHIPCONTROLSYSTEM_H
 
+#include "hecate/ComponentMapper.h"
+#include "hecate/EntityProcessingSystem.h"
+
+#include "SDL.h"
+
 namespace StarWarrior {
 
-class PlayerShipControlSystem {
+class Transform;
 
+class PlayerShipControlSystem : public hecate::EntityProcessingSystem {
+private:
+	bool moveRight;
+	bool moveLeft;
+	bool shoot;
+	hecate::ComponentMapper<Transform> transformMapper;
+
+public:
+	PlayerShipControlSystem();
+	void initialize();
+	void keyPressed(SDLKey key);
+	void keyReleased(SDLKey key);
+
+protected:
+	void process(hecate::Entity *e);
 };
 
 }
--- src/systems/PlayerShipControlSystem.cpp
+++ src/systems/PlayerShipControlSystem.cpp
@@ -1,1 +1,70 @@
 #include "systems/PlayerShipControlSystem.h"
+
+#include "EntityFactory.h"
+#include "components/Player.h"
+#include "components/Transform.h"
+#include "components/Velocity.h"
+
+#include "hecate/Entity.h"
+
+using namespace hecate;
+
+namespace StarWarrior {
+
+PlayerShipControlSystem::PlayerShipControlSystem() :
+transformMapper(Transform(), world) {
+	Player p;
+	Transform t;
+	componentList_t l;
+	l.push_back(&t);
+	l.push_back(&p);
+	setupTypes(l);
+}
+
+void PlayerShipControlSystem::initialize() {
+}
+
+void PlayerShipControlSystem::keyPressed(SDLKey key) {
+	if (key == SDLK_a) {
+		moveLeft = true;
+		moveRight = false;
+	} else if (key == SDLK_d) {
+		moveRight = true;
+		moveLeft = false;
+	} else if (key == SDLK_SPACE) {
+		shoot = true;
+	}
+}
+
+void PlayerShipControlSystem::keyReleased(SDLKey key) {
+	if (key == SDLK_a) {
+		moveLeft = false;
+	} else if (key == SDLK_d) {
+		moveRight = false;
+	} else if (key == SDLK_SPACE) {
+		shoot = false;
+	}
+}
+
+void PlayerShipControlSystem::process(Entity *e) {
+	Transform *transform = transformMapper.get(*e);
+
+	if (moveLeft) {
+		transform->addX(world->getDelta() * -0.3f);
+	}
+	if (moveRight) {
+		transform->addX(world->getDelta() * 0.3f);
+	}
+
+	if (shoot) {
+		Entity *missile = EntityFactory::createMissile(world);
+		missile->getComponent(Transform())->setLocation(transform->getX(), transform->getY() - 20);
+		missile->getComponent(Velocity())->setVelocity(-0.5f);
+		missile->getComponent(Velocity())->setAngle(90);
+		missile->refresh();
+
+		shoot = false;
+	}
+}
+
+}
