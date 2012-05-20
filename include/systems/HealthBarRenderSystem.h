17c32dacff0c14754782da0d83965b7d3874f1d5
Harley Laue <losinggeneration@gmail.com>
2012-05-20 06:36:54 -0500

    Implement HealthBarRenderSystem
    * Now we require SDL_ttf for rendering the healt text

--- include/systems/HealthBarRenderSystem.h
+++ include/systems/HealthBarRenderSystem.h
@@ -1,10 +1,30 @@
 #ifndef STARWARRIOR_HEALTHBARRENDERSYSTEM_H
-#define STARWARRIOR_
+#define STARWARRIOR_HEALTHBARRENDERSYSTEM_H
+
+#include "hecate/ComponentMapper.h"
+#include "hecate/EntityProcessingSystem.h"
+
+#include "SDL.h"
+#include "SDL_ttf.h"
 
 namespace StarWarrior {
 
-class HealthBarRenderSystem {
+class Health;
+class Transform;
 
+class HealthBarRenderSystem : public hecate::EntityProcessingSystem {
+public:
+	HealthBarRenderSystem(SDL_Surface *screen, TTF_Font *font);
+	void initialize();
+
+protected:
+	void process(hecate::Entity *e);
+
+private:
+	SDL_Surface *screen;
+	TTF_Font *font;
+	hecate::ComponentMapper<Health> healthMapper;
+	hecate::ComponentMapper<Transform> transformMapper;
 };
 
 }
--- src/systems/HealthBarRenderSystem.cpp
+++ src/systems/HealthBarRenderSystem.cpp
@@ -1,1 +1,48 @@
 #include "systems/HealthBarRenderSystem.h"
+
+#include "components/Health.h"
+#include "components/Transform.h"
+
+#include <sstream>
+
+using namespace hecate;
+
+namespace StarWarrior {
+
+HealthBarRenderSystem::HealthBarRenderSystem(SDL_Surface *screen, TTF_Font *font) :
+healthMapper(Health(), world), transformMapper(Transform(), world) {
+	Health h;
+	Transform t;
+	componentList_t l;
+
+	l.push_back(&h);
+	l.push_back(&t);
+	setupTypes(l);
+
+	this->screen = screen;
+	this->font = font;
+}
+
+void HealthBarRenderSystem::initialize() {
+}
+
+void HealthBarRenderSystem::process(Entity *e) {
+	Health *health = healthMapper.get(*e);
+	Transform *transform = transformMapper.get(*e);
+	std::string text;
+	SDL_Color white = {255, 255, 255};
+	SDL_Rect r;
+
+	std::stringstream ss;
+	ss << health->getHealthPercentage() << "%";
+	ss >> text;
+
+	SDL_Surface *rendered = TTF_RenderText_Solid(font, text.c_str(), white);
+	r.w = rendered->w;
+	r.h = rendered->h;
+	r.x = transform->getX() - 10;
+	r.y = transform->getY() - 30;
+	SDL_BlitSurface(screen, &r, rendered, NULL);
+}
+
+}
