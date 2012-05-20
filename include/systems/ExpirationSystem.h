68f287ee06ccc67d5777ecd8493bc5c8ccc37248
Harley Laue <losinggeneration@gmail.com>
2012-05-20 06:14:15 -0500

    Implement ExpirationSystem

--- include/systems/ExpirationSystem.h
+++ include/systems/ExpirationSystem.h
@@ -1,10 +1,23 @@
 #ifndef STARWARRIOR_EXPIRATIONSYSTEM_H
-#define STARWARRIOR_
+#define STARWARRIOR_EXPIRATIONSYSTEM_H
+
+#include "hecate/ComponentMapper.h"
+#include "hecate/EntityProcessingSystem.h"
 
 namespace StarWarrior {
 
-class ExpirationSystem {
+class Expires;
 
+class ExpirationSystem : public hecate::EntityProcessingSystem {
+public:
+	ExpirationSystem();
+	void initialize();
+
+protected:
+	void process(hecate::Entity *e);
+
+private:
+	hecate::ComponentMapper<Expires> expiresMapper;
 };
 
 }
--- src/systems/ExpirationSystem.cpp
+++ src/systems/ExpirationSystem.cpp
@@ -1,1 +1,28 @@
 #include "systems/ExpirationSystem.h"
+
+#include "components/Expires.h"
+
+using namespace hecate;
+
+namespace StarWarrior {
+
+ExpirationSystem::ExpirationSystem() : expiresMapper(Expires(), world) {
+	Expires e;
+	componentList_t l;
+	l.push_back(&e);
+	setupTypes(l);
+}
+
+void ExpirationSystem::initialize() {
+}
+
+void ExpirationSystem::process(Entity *e) {
+	Expires *expires = expiresMapper.get(*e);
+	expires->reduceLifeTime(world->getDelta());
+
+	if (expires->isExpired()) {
+		world->deleteEntity(e);
+	}
+}
+
+}
