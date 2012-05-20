874dc0d5c2c6b3c31e758f80f05f625c61ddf3e1
Harley Laue <losinggeneration@gmail.com>
2012-05-20 14:43:57 -0500

    Implemented MovementSystem

--- include/systems/MovementSystem.h
+++ include/systems/MovementSystem.h
@@ -1,10 +1,25 @@
 #ifndef STARWARRIOR_MOVEMENTSYSTEM_H
 #define STARWARRIOR_MOVEMENTSYSTEM_H
 
+#include "hecate/ComponentMapper.h"
+#include "hecate/EntityProcessingSystem.h"
+
 namespace StarWarrior {
 
-class MovementSystem {
+class Transform;
+class Velocity;
 
+class MovementSystem : public hecate::EntityProcessingSystem {
+public:
+	MovementSystem();
+	void initialize();
+
+protected:
+	void process(hecate::Entity *e);
+
+private:
+	hecate::ComponentMapper<Velocity> velocityMapper;
+	hecate::ComponentMapper<Transform> transformMapper;
 };
 
 }
--- src/systems/MovementSystem.cpp
+++ src/systems/MovementSystem.cpp
@@ -1,1 +1,38 @@
 #include "systems/MovementSystem.h"
+
+#include "components/Transform.h"
+#include "components/Velocity.h"
+
+#include <cmath>
+
+using namespace hecate;
+
+namespace StarWarrior {
+
+MovementSystem::MovementSystem() : transformMapper(Transform(), world),
+velocityMapper(Velocity(), world) {
+	Transform t;
+	Velocity v;
+	componentList_t l;
+	l.push_back(&t);
+	l.push_back(&v);
+	setupTypes(l);
+}
+
+void MovementSystem::initialize() {
+}
+
+void MovementSystem::process(Entity *e) {
+	Velocity *velocity = velocityMapper.get(*e);
+	float r = velocity->getAngleAsRadians();
+	float v = velocity->getVelocity();
+
+	Transform *transform = transformMapper.get(*e);
+
+	float xn = transform->getX() + (cos(r) * v * world->getDelta());
+	float yn = transform->getY() + (sin(r) * v * world->getDelta());
+
+	transform->setLocation(xn, yn);
+}
+
+}
