54882ad1c643376558d5b35a067bf428eceef1fe
Harley Laue <losinggeneration@gmail.com>
2012-05-20 15:38:10 -0500

    Implemented RenderSystem

--- include/systems/RenderSystem.h
+++ include/systems/RenderSystem.h
@@ -1,10 +1,38 @@
 #ifndef STARWARRIOR_RENDERSYSTEM_H
 #define STARWARRIOR_RENDERSYSTEM_H
 
+#include "hecate/ComponentMapper.h"
+#include "hecate/EntityProcessingSystem.h"
+
+#include "SDL.h"
+
 namespace StarWarrior {
 
-class RenderSystem {
+class Spatial;
+class SpatialForm;
+class Transform;
 
+class RenderSystem : hecate::EntityProcessingSystem {
+public:
+	RenderSystem(SDL_Surface *screen, int width, int height);
+	void initialize();
+
+protected:
+	void process(hecate::Entity *e);
+	void added(hecate::Entity *e);
+	void removed(hecate::Entity *e);
+
+private:
+	typedef std::map<int, Spatial*> spatialMap_t;
+	spatialMap_t spatials;
+	hecate::ComponentMapper<SpatialForm> spatialFormMapper;
+	hecate::ComponentMapper<Transform> transformMapper;
+	SDL_Surface *screen;
+
+	int width;
+	int height;
+
+	Spatial *createSpatial(hecate::Entity *e);
 };
 
 }
--- src/systems/RenderSystem.cpp
+++ src/systems/RenderSystem.cpp
@@ -1,1 +1,81 @@
 #include "systems/RenderSystem.h"
+
+#include "components/SpatialForm.h"
+#include "components/Transform.h"
+
+#include "spatials/EnemyShip.h"
+#include "spatials/Explosion.h"
+#include "spatials/Missile.h"
+#include "spatials/PlayerShip.h"
+#include "spatials/Spatial.h"
+
+#include "SDL.h"
+#include <cstring>
+
+using namespace hecate;
+
+namespace StarWarrior {
+
+RenderSystem::RenderSystem(SDL_Surface *screen, int width, int height) :
+spatialFormMapper(SpatialForm(""), world), transformMapper(Transform(), world) {
+	SpatialForm sf("");
+	Transform t;
+	componentList_t l;
+	l.push_back(&t);
+	l.push_back(&sf);
+	setupTypes(l);
+
+	this->screen = screen;
+	this->width = width;
+	this->height = height;
+}
+
+void RenderSystem::initialize() {
+}
+
+void RenderSystem::process(Entity *e) {
+	spatialMap_t::iterator it = spatials.find(e->getId());
+	Spatial *spatial = NULL;
+	Transform *transform = transformMapper.get(*e);
+
+	if(it != spatials.end()) {
+		spatial = it->second;
+	}
+
+	if (transform->getX() >= 0 && transform->getY() >= 0 && transform->getX() < width && transform->getY() < height && spatial != NULL) {
+		spatial->render(screen);
+	}
+}
+
+void RenderSystem::added(Entity *e) {
+	Spatial *spatial = createSpatial(e);
+	if (spatial != NULL) {
+		spatial->initialize();
+		spatials[e->getId()] = spatial;
+	}
+}
+
+void RenderSystem::removed(Entity *e) {
+	spatials.erase(e->getId());
+}
+
+Spatial *RenderSystem::createSpatial(Entity *e) {
+	SpatialForm *spatialForm = spatialFormMapper.get(*e);
+	std::string spatialFormFile = spatialForm->getSpatialFormFile();
+
+	if (strcasecmp("PlayerShip", spatialFormFile.c_str()) == 0) {
+		return new PlayerShip(world, e);
+	} else if (strcasecmp("Missile", spatialFormFile.c_str()) == 0) {
+		return new Missile(world, e);
+	} else if (strcasecmp("EnemyShip", spatialFormFile.c_str()) == 0) {
+		return new EnemyShip(world, e);
+	} else if (strcasecmp("BulletExplosion", spatialFormFile.c_str()) == 0) {
+		return new Explosion(world, e, 10);
+	} else if (strcasecmp("ShipExplosion", spatialFormFile.c_str()) == 0) {
+		return new Explosion(world, e, 30);
+	}
+
+	return NULL;
+}
+
+}
